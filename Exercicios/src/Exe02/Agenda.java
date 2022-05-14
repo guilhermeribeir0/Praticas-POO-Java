79215c915e785419c4798277e5d31692367f01e6
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-05-14 13:00:34 -0300

    adicionado, classes, e primeiras configuracoes

---
+++ Exercicios/src/Exe02/Agenda.java
@@ -0,0 +1,36 @@
+package Exe02;
+
+import java.util.ArrayList;
+
+public class Agenda extends Pessoa{
+
+    private String nomeInformado;
+    private int idadeInformada;
+
+    ArrayList nomePessoa = new ArrayList<String>();
+    ArrayList idadePessoa = new ArrayList<String>();
+
+    public void setNomeInformado(String nomeInformado) {
+        this.nomeInformado = nomeInformado;
+    }
+
+    public String getNomeInformado() {
+        return nomeInformado;
+    }
+
+    public void setIdadeInformada(int idadeInformada) {
+        this.idadeInformada = idadeInformada;
+    }
+
+    public int getIdadeInformada() {
+        return idadeInformada;
+    }
+
+    public void adicionaPessoa() {
+        nomePessoa.add(getNome());
+    }
+
+    public void removePessoa () {
+        idadePessoa.remove(idadeInformada);
+    }
+}
--- Exercicios/src/Exe02/Principal.java
+++ Exercicios/src/Exe02/Principal.java
@@ -1,8 +1,18 @@
 package Exe02;
 
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
+
+        Pessoa pessoa1 = new Pessoa();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Olá, bem vindo ao sistema de Cadastro de Pessoas");
+        System.out.println("Escolha o numero correspondente a opção desejada: ");
+
 
     }
 
