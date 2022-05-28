f2f1d1aff359c4ff87bf4a2a07bd669a86228604
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-05-28 16:31:09 -0300

    Organizando as classes.

---
+++ Exercicios/src/Exe02/Pessoa.java
@@ -0,0 +1,32 @@
+package Exe02;
+
+public class Pessoa {
+
+        private String nome;
+        private Integer idade;
+        private Float altura;
+
+        public String getNome() {
+                return nome;
+        }
+
+        public void setNome(String nome) {
+                this.nome = nome;
+        }
+
+        public Integer getIdade() {
+                return idade;
+        }
+
+        public void setIdade(Integer idade) {
+                this.idade = idade;
+        }
+
+        public Float getAltura() {
+                return altura;
+        }
+
+        public void setAltura(Float altura) {
+                this.altura = altura;
+        }
+}
