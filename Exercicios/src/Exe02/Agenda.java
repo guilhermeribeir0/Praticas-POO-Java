866882f41d75930bc5f8dc4190c957888309e3f2
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-05-28 12:06:57 -0300

    Recomecando.

--- Exercicios/src/Exe02/Agenda.java
+++ Exercicios/src/Exe02/Agenda.java
@@ -2,16 +2,6 @@ package Exe02;
 
 public class Agenda {
 
-    public void armazenaPessoa (String nome, int idade, float altura) {}
 
-    public void removePessoa (String nome) {}
-
-    public int buscaPessoa() {
-        return 0;
-    }
-
-    public void imprimeAgenda () {}
-
-    public void imprimePessoa (int index) {}
 
 }
