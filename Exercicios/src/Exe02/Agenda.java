53fd4a2a2344c7c8f095e938c2dfa0e66a8bba65
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-05-29 12:09:14 -0300

    Adicionado funcoes e configurado a funcao adicionarPessoa.

--- Exercicios/src/Exe02/Agenda.java
+++ Exercicios/src/Exe02/Agenda.java
@@ -1,7 +1,76 @@
 package Exe02;
 
-public class Agenda {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Agenda{
 
+    private String[] nome = new String[10];
+    private int[] idade = new int[10];
+    private float[] altura = new float[10];
 
+    public String[] getNome() {
+        return nome;
+    }
+
+    public void setNome(String[] nome) {
+        this.nome = nome;
+    }
+
+    public int[] getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int[] idade) {
+        this.idade = idade;
+    }
+
+    public float[] getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float[] altura) {
+        this.altura = altura;
+    }
+
+    public void armazenaPessoa(String nome[], int idade[], float altura[]) {
+            this.nome = nome;
+            this.idade = idade;
+            this.altura = altura;
+    }
+
+    public void removePessoa(String nome) {
+
+    }
+
+    //Temporario
+    public int buscaPessoa(String nome) {
+        return 0;
+    }
+
+    public void imprimeAgenda() {
+        System.out.println(" ID |  NOME  |  IDADE  |  ALTURA ");
+        System.out.println("---------------------------------");
+        for (int i = 0; i < nome.length; i++) {
+            if(nome[i] == null) {
+                System.out.println(i + " " + "Não Preenchido!  |   0   |   0,00  ");
+            } else {
+                System.out.println(i + " | " + nome[i] + " | " + idade[i] + " | " + altura[i]);
+            }
+        }
+    }
+
+    public void imprimePessoa(int index) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Agenda{" +
+                "nome=" + Arrays.toString(nome) +
+                ", idade=" + Arrays.toString(idade) +
+                ", altura=" + Arrays.toString(altura) +
+                '}';
+    }
 }
--- Exercicios/src/Exe02/Principal.java
+++ Exercicios/src/Exe02/Principal.java
@@ -8,8 +8,51 @@ public class Principal {
 
         Scanner input = new Scanner(System.in);
 
+        Agenda agenda = new Agenda();
 
+        String[] nomeInformado = new String[10];
+        int[] idadeInformada = new int[10];
+        float[] alturaInformada = new float[10];
+        int escolha;
 
+        System.out.println("---- Bem Vindo(a) a Agenda ----");
+        System.out.println("1 - Adicionar Pessoa \n2 - Remover Pessoa \n3 - Buscar Pessoa \n4 - Listar (Tudo) \n5 - Listar (Especifico) \n6 - Sair");
+        System.out.print("Informe uma das opções acima: ");
+        escolha = input.nextInt();
+
+        if (escolha < 6) {
+            switch (escolha) {
+                case 1:
+                    System.out.println("Informe quantas pessoas quer cadastrar: ");
+                    int quantidadePessoas = input.nextInt();
+                    for (int i = 0; i < quantidadePessoas; i++) {
+                        if (nomeInformado[i] == null ) {
+                            System.out.print("Informe o nome: ");
+                            nomeInformado[i] = input.next();
+                            System.out.print("Informe a idade: ");
+                            idadeInformada[i] = input.nextInt();
+                            System.out.print("Informe a altura: ");
+                            alturaInformada[i] = input.nextFloat();
+                            agenda.armazenaPessoa(nomeInformado, idadeInformada, alturaInformada);
+                        }
+                    }
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+            }
+            agenda.imprimeAgenda();
+        }
+        System.out.println("-------------------- \nPrograma Finalizado! \n--------------------");
     }
 
 }
