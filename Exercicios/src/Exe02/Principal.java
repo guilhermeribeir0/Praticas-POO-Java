943224c6a0369af0f7ab327e045ebed7763d9b95
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-05-31 20:56:08 -0300

    Ajustes no primeiro 'for' da classe Principal e outros ajustes.

--- Exercicios/src/Exe02/Principal.java
+++ Exercicios/src/Exe02/Principal.java
@@ -17,7 +17,7 @@ public class Principal {
 
         System.out.println("---- Bem Vindo(a) a Agenda ----");
 
-        for (int i = escolha; escolha < 6; i++) {
+        for (int i = 0; escolha != 6; i++) {
             System.out.println("1 - Adicionar Pessoa \n2 - Remover Pessoa \n3 - Buscar Pessoa \n4 - Listar (Tudo) \n5 - Listar (Especifico) \n6 - Sair");
             System.out.print("Informe uma das opções acima: ");
             escolha = input.nextInt();
@@ -37,26 +37,26 @@ public class Principal {
                         alturaInformada = input.nextFloat();
                         agenda.armazenaPessoa(nomeInformado, idadeInformada, alturaInformada);
                     }
-                    System.out.println("-------------------- \nConcluido com sucesso \n--------------------");
+                    System.out.println("****( Concluido com sucesso )****\n-------------------------");
                     break;
 
                 case 2:
                     System.out.print("Informe o nome da pessoa: ");
                     String pessoaRemover = input.next();
                     agenda.removePessoa(pessoaRemover);
-                    System.out.println("-------------------- \nConcluido com sucesso \n--------------------");
+                    System.out.println("****( Concluido com sucesso )****\n-------------------------");
                     break;
 
                 case 3:
                     System.out.print("Informe o nome da pessoa: ");
                     String pessoaPesquisada = input.next();
                     agenda.buscaPessoa(pessoaPesquisada);
-                    System.out.println("-------------------- \nConcluido com sucesso \n--------------------");
+                    System.out.println("****( Concluido com sucesso )****\n-------------------------");
                     break;
 
                 case 4:
                     agenda.imprimeAgenda();
-                    System.out.println("-------------------- \nConcluido com sucesso \n--------------------");
+                    System.out.println("****( Concluido com sucesso )****\n-------------------------");
                     break;
 
                 case 5:
@@ -66,7 +66,7 @@ public class Principal {
                     break;
 
                 case 6:
-                    System.out.println("-------------------- \nPrograma Finalizado! \n--------------------");
+                    System.out.println("****( Concluido com sucesso )****\n-------------------------");
                     break;
             }
         }
