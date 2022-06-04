08e6e1c525a827d1cc09b39951f1d17fd6b30192
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-06-04 11:13:43 -0300

    Criado os packages dos proximos exercicios.

---
+++ Exercicios/src/Exe03/Principal.java
@@ -0,0 +1,11 @@
+package Exe03;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ Exercicios/src/Exe04/Principal.java
@@ -0,0 +1,11 @@
+package Exe04;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
